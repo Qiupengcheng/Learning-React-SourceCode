44ac56b24b24f6b90906ef458a6d7bd2eb1e6506
Szqpc <596296060@qq.com>
2018-03-26 10:05:36 +0800

    save

---
+++ note/面试笔记/js/travel.js
@@ -0,0 +1,25 @@
+const tree = {
+  value: 1-1,
+  children: [
+    {
+      value: 2-1,
+      chldren: [
+        {
+          value: 3-1
+        },
+        {
+          value: 3-2
+        }
+      ]
+    },
+    {
+      value: 2-2
+    }
+  ]
+}
+
+/**
+ * 1-1, 2-1, 2-2, 3-1, 3-2
+ */
+
+// 不用递归
